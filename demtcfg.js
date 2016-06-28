822ff35e6d4273fa230ef73310f53a65f2f85cc9
finnyu2015 <finnyu2015@gmail.com>
2016-06-28 15:32:19 +0800

    Update demtcfg.js
    add demtcfg.erpHostTest

--- demtcfg.js
+++ demtcfg.js
@@ -1,17 +1,18 @@
 var demtcfg = {} ;
-//	demtcfg.bridge = "false" ;
-	demtcfg.bridge = "true" ;
+//demtcfg.bridge = "false" ;
+ demtcfg.bridge = "true" ;
 
 demtcfg.isBridge=function(){
 	return demtcfg.bridge==='true' ;
 }
 
-demtcfg.zipcode = false ;
+demtcfg.zipcode = (demtcfg.bridge !== "false") ;
 demtcfg.portalHref="icscApp.html#demjPortal";
 
 
 //demtcfg.loginHost =  window.location.pathname.split("/")[1];
-demtcfg.erpHost =  "";
+demtcfg.erpHost = "";
+demtcfg.erpHostTest = "http://10.1.198.242";
 demtcfg.lite = true ;
 
 //demtcfg.erpHost =  "http://groupeipt.csc.com.tw";
